f93354021289e4c3c6fc619930c88b65286d10fa
LoboEsparta <a3519110228@alumno.uttehuacan.edu.mx>
2022-10-12 19:32:24 -0500

    update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container" mt-3>
+
+    <Header></Header>
+    <Formulario></Formulario>
+
+    </div>
+</template>
+
+
+
+
+<script>
+    import Header from "./components/Header.vue"
+    import Formulario from "./components/Formularios.vue"
+
+    export default{
+        components: {
+            Header,
+            Formulario,
+        }
+    }
+</script>
+
+
+
+
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+     <div class="container mt-3">
+            <div class="row">
+                 <div class="col-12 col-md-3">
+                    <h1>Formularios</h1>
+                 </div>
+                 <div class="col-12 col-md-6">
+                    <img width="100" :src="img" alt="Vue.js">
+                 </div>
+            </div>
+     </div>
+            <hr>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            img: "https://cdn.svgporn.com/logos/vue.svg",
+        }),
+       
+    };
+</script>
