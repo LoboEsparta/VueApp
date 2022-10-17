7c63b44eef9e51d136a50f4ad5f7db28d6f01468
LoboEsparta <a3519110228@alumno.uttehuacan.edu.mx>
2022-10-17 17:52:24 -0500

    Examen

--- src/components/Formularios.vue
+++ src/components/Formularios.vue
@@ -1,15 +1,13 @@
 <template>
-  
   <div class="row">
 
     <div class="col-12 mb-4">
       <Porgressbar :porcentaje="porcentaje"></Porgressbar>
     </div>
 
-
-
     <div class="col-12 col-md-4">
       <form @submit.prevent="registrarProyecto">
+
    <div class="mb-3">
       <label class="form-label">Proyecto</label>
       <input v-model.trim="proyecto" type="text" class="form-control" required>
@@ -26,24 +24,22 @@
       </div>
 
     <div class="mb-3">
-        <label for="exampleInoutPassword1" class="form-check-label">Urgente</label>
+        <label  class="form-check-label">Urgente</label>
         <input v-model="urgente" type="checkbox" class="form-check-input" />
       </div>
-
 
       <button type="submit" class="btn btn-primary">Guardar</button>
   </form>
     </div>
 
-
-
-
     <div class="col-12 col-md-8">
     <Total-proyecs
     :numeroProyectos="numeroProyectos" 
     :proyectos="proyectos" 
-    :cambiarEstado="cambiarEstado"/>
-  
+    :cambiarEstado="cambiarEstado"
+    :limpiarData="limpiarData"
+    :limpiar="limpiar"
+  />
     </div>
   </div>
 
@@ -70,17 +66,35 @@ import Porgressbar from "./Porgressbar.vue";
         completado: false,
       };
       this.proyectos.push(proyecto);
-      localStorage.setItem("proyectos", JSON.stringify(this.proyectos));
-
-      
-
+      this.saveData();
       this.proyecto="";
       this.tipo ="";
       this.urgente = false ;
     },
+
+
     cambiarEstado(proyecto, campo){
       proyecto[campo] = !proyecto[campo];
       //this.proyectos[id].urgente = !this.proyectos[id].urgente;
+      this.saveData();
+    },
+
+
+    saveData(){
+      localStorage.setItem("proyectos", JSON.stringify(this.proyectos));
+    },
+
+
+    limpiarData(){
+      this.proyectos = [];
+      localStorage.clear();
+      },
+
+    limpiar(proyectos,index){
+      this.proyectos[index]="";
+                this.proyectos.splice(index,1)
+                localStorage.clear(proyectos.index);
+                this.saveData();
     }
   },
   computed: {
--- src/components/TotalProyecs.vue
+++ src/components/TotalProyecs.vue
@@ -1,5 +1,12 @@
 <template>
-      <h3>Total de proyectos: {{numeroProyectos}}</h3>
+      <div class="row my-3 ">
+        <div class="col-6">
+          <h3>Total de proyectos: {{numeroProyectos}}</h3>
+        </div>
+        <div class="col-6 text-end">
+           <button @click="limpiarData" class="btn btn-danger">Limpiar proyectos</button>
+        </div>
+      </div>
 
     <div class="table-responsive">
     <table class="table table-dark">
@@ -10,6 +17,7 @@
           <th>Tipo</th>
           <th>Urgente</th>
           <th>Completado</th>
+          <th>Borrar</th>
         </tr>
       </thead>
       <tbody>
@@ -17,9 +25,14 @@
           <td>{{index}}</td>
           <td>{{proyecto.proyecto}}</td>
           <td>{{proyecto.tipo}}</td>
-          <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{proyecto.urgente ? "Si" : "No"}}</td>
-          <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{proyecto.completado ? "Completado" : "Incompleto"}}</td>
+          <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ?
+           'bg-success' : 'bg-danger'">{{proyecto.urgente ? "Si" : "No"}}</td>
+          <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ?
+           'bg-success' : 'bg-danger'">{{proyecto.completado ? "Completado" : "Incompleto"}}</td>
        
+          <td>
+            <button @click="limpiar(proyecto,index)" class="btn btn-danger">Eliminar</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -28,7 +41,9 @@
 
 <script>
 export default{
-    props: ["numeroProyectos", "proyectos", "cambiarEstado"],
-}
+    props: ["numeroProyectos", "proyectos", "cambiarEstado","limpiarData","limpiar",
+    ],
+    
+};
 
 </script>
